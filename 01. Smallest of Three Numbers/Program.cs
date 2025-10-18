94f6e3e1e4de920e700a34feb203d09cf80cd858
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-18 10:27:04 +0300

    Add project files.

---
+++ 01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+static int SmallestOfThreeNumbers(int a, int b, int c)
+{
+    int smallest = a;
+    if (b < smallest)
+    {
+        smallest = b;
+    }
+    if (c < smallest)
+    {
+        smallest = c;
+    }
+    return smallest;
+}
+Console.WriteLine(SmallestOfThreeNumbers(num1, num2, num3));
