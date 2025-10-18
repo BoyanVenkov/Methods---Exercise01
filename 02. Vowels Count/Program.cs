e4b6de31c8ce6050eeb984ab4f3415657f246d9e
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-18 10:37:49 +0300

    Added a New Project

---
+++ 02. Vowels Count/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        VowelCount(input);
+    }
+
+    static void VowelCount(string text)
+    {
+        int count = 0;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if ("aeiouAEIOU".Contains(text[i]))
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+}
