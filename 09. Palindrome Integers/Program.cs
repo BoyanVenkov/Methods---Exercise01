73b6f0d396fbc7194ee089cf4ed7c7e6126edf5c
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-19 18:35:49 +0300

    Added a New Project

---
+++ 09. Palindrome Integers/Program.cs
@@ -0,0 +1,25 @@
+﻿string number = string.Empty;
+
+PalindromeCheck(number);
+
+static void PalindromeCheck(string num)
+{
+    string number = Console.ReadLine();
+    while (number != "END")
+    {
+        
+        string output = string.Empty;
+        string compareNum = new string(number.Reverse().ToArray());
+        if (number == compareNum)
+        {
+            output = "true";
+        }
+        else
+        {
+            output = "false";
+        }
+
+        Console.WriteLine(output);
+        number = Console.ReadLine();
+    }
+}
