7a5e85ada8e066f39668369ce6467cb0d9f54301
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-19 16:38:28 +0300

    Added a New Project

---
+++ 05. Add and Subtract/Program.cs
@@ -0,0 +1,24 @@
+﻿int firstNum = int.Parse(Console.ReadLine());
+int secondNum = int.Parse(Console.ReadLine());
+int thirdNum = int.Parse(Console.ReadLine());
+
+int firstOperation = IntegerCalculations(firstNum, secondNum);
+
+
+static int IntegerCalculations(int num1, int num2)
+{
+    int sum = num1 + num2;
+
+    return sum;
+
+}
+
+static int IntegerFinalResult(int num1, int num2)
+{
+    int totalSum = 0;
+    totalSum = num1 - num2;
+
+    return totalSum;
+}
+
+Console.WriteLine(IntegerFinalResult(firstOperation, thirdNum));
