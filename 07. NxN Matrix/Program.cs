4a6aa44f5be23fd5d37cc70cfd9e70e134157185
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-19 16:59:56 +0300

    Added a New Project

---
+++ 07. NxN Matrix/Program.cs
@@ -0,0 +1,21 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+NxNMethodPrint(number);
+
+
+static void NxNMethodPrint(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        for (int j = 1; j <= num; j++)
+        {
+            if (j == num)
+            {
+                Console.WriteLine($"{num}");
+                break;
+            }
+            Console.Write($"{num} ");
+        }
+    }
+
+}
