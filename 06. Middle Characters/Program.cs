5ef07cb2beef2cec60280f1be2b74e2d4b7d8939
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-19 16:53:49 +0300

    Added a New Project

---
+++ 06. Middle Characters/Program.cs
@@ -0,0 +1,26 @@
+﻿string input = Console.ReadLine();
+
+FindTheMiddleChar(input);
+
+static void FindTheMiddleChar(string word)
+{
+    char middleChar = ' ';
+    char middleCharTwo = ' ';
+    string middleString = " ";
+    if (word.Length % 2 != 0)
+    {
+        middleChar = word[word.Length / 2];
+        middleString = ($"{middleChar}");
+    }
+    else
+    {
+        middleCharTwo = word[(word.Length / 2) - 1];
+        middleChar = word[word.Length / 2];
+        middleString = ($"{middleCharTwo}{middleChar}");
+    }
+
+    Console.WriteLine(middleString);
+}
+
+
+
