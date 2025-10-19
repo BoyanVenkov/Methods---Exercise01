a8e8d900054013d9f87fdb1f49935b56bebbddc8
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-19 15:33:35 +0300

    Added a New Project

---
+++ 03. Characters in Range/Program.cs
@@ -0,0 +1,27 @@
+﻿char firstChar = char.Parse(Console.ReadLine());
+char secondChar = char.Parse(Console.ReadLine());
+
+CharOutput(firstChar, secondChar);
+
+static void CharOutput(char a, char b)
+{
+
+    if (b < a)
+    {
+        char temp = a;
+        a = b;
+        b = temp;
+        for (int i = a + 1; i < b; i++)
+        {
+            Console.Write($"{(char)(i)} ");
+        }
+        return;
+    }
+    for (int i = a + 1; i < b; i++)
+    {
+        Console.Write($"{(char)(i)} ");
+    }
+
+}
+
+
