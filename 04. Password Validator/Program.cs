0fe9867dbdea50cc9ed01471787039ea8dec7848
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-19 15:55:12 +0300

    Added a New Project

---
+++ 04. Password Validator/Program.cs
@@ -0,0 +1,31 @@
+﻿string password = Console.ReadLine();
+
+PasswordValidator(password);
+
+
+static void PasswordValidator(string input)
+{
+    int digitCount = input.Count(char.IsDigit);
+    bool isValid = true;
+
+    if (input.Length < 6 || input.Length > 10)
+    {
+        Console.WriteLine("Password must be between 6 and 10 characters");
+        isValid = false;
+    }
+    if (input.Any(c => !char.IsLetterOrDigit(c)))
+    {
+        Console.WriteLine("Password must consist only of letters and digits");
+        isValid = false;
+    }
+    if (digitCount < 2)
+    {
+        Console.WriteLine("Password must have at least 2 digits");
+        isValid = false;
+    }
+    if (isValid == true)
+    {
+        Console.WriteLine("Password is valid");
+    }
+
+}
