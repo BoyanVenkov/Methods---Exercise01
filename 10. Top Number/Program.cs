9d52df28e501a03b9aa1e092861e4847b6033fed
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-20 06:28:11 +0300

    Added a New Project

---
+++ 10. Top Number/Program.cs
@@ -0,0 +1,34 @@
+﻿int endValue = int.Parse(Console.ReadLine());
+
+TopNumber(endValue);
+
+
+static void TopNumber(int num)
+{
+    for (int i = 17; i <= num; i++)
+    {
+        int sum = 0;
+        int numI = i;
+        int oddNum = 0;
+        int lastNum = 0;
+        while (numI > 0)
+        {
+            lastNum += numI % 10;
+            sum += numI % 10;
+            if (lastNum % 2 != 0)
+            {
+                oddNum += numI;
+            }
+            numI /= 10;
+
+        }
+        if (sum % 8 == 0 && oddNum > 0)
+        {
+            Console.WriteLine(i);
+        
+        }
+
+    }
+
+
+}
---
+++ 11. Array Manipulator/Program.cs
@@ -0,0 +1,102 @@
+﻿int[] arrayIntegers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+string[] input = Console.ReadLine().Split().ToArray();
+
+while (input[0] != "end")
+{
+
+    if (input[0] == "exchange")
+    {
+        if (int.Parse(input[1]) < 0 || int.Parse(input[1]) >= arrayIntegers.Length)
+        {
+            Console.WriteLine("Invalid index");
+            break;
+        }
+        int count = 0;
+        int index = 0;
+        int[] subArray = arrayIntegers.ToArray();
+        for (int i = int.Parse(input[1]) + 1; i < arrayIntegers.Length; i++)
+        {
+            subArray[index] = arrayIntegers[i];
+            index++;
+        }
+        for (int i = index; i < arrayIntegers.Length; i++)
+        {
+
+            subArray[i] = arrayIntegers[count];
+            count++;
+        }
+        arrayIntegers = subArray.ToArray();
+    }
+    if (input[0] == "max")
+    {
+        if (input[1] == "even")
+        {
+            int maxEven = 0;
+            int maxEvenIndex = 0;
+            for (int i = 0; i < arrayIntegers.Length; i++)
+            {
+                if (arrayIntegers[i] % 2 == 0 && arrayIntegers[i] >= maxEven)
+                { 
+                    maxEven = arrayIntegers[i];
+                    maxEvenIndex = i;
+                }
+            }
+            if (maxEvenIndex == 0)
+            {
+                Console.WriteLine("No matches");
+                break;
+            }
+            Console.WriteLine(maxEvenIndex);
+        }
+        if (input[1] == "odd")
+        {
+            int maxOdd = 0;
+            int maxOddIndex = 0;
+            for (int i = 0; i < arrayIntegers.Length; i++)
+            {
+                if (arrayIntegers[i] % 2 != 0 && arrayIntegers[i] >= maxOdd)
+                {
+                    maxOdd = arrayIntegers[i];
+                    maxOddIndex = i;
+                }
+            }
+            if (maxOddIndex == 0)
+            {
+                Console.WriteLine("No matches");
+                break;
+            }
+            Console.WriteLine(maxOddIndex);
+        }
+    }
+    if (input[0] == "min")
+    {
+
+    }
+    if (input[0] == "first")
+    {
+        if (input[2] == "even")
+        {
+
+        }
+        if (input[2] == "odd")
+        {
+
+        }
+    }
+    if (input[0] == "last")
+    {
+        if (input[2] == "even")
+        {
+
+        }
+        if (input[2] == "odd")
+        {
+
+        }
+    }
+
+
+
+
+    input = Console.ReadLine().Split().ToArray();
+}
